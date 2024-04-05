beb2aac3e96f0dc8623055c0c0bd856ad8fa9029
BOUCHENOT GAELLE p1935539 <gaelle.bouchenot@etu.univ-lyon1.fr>
2024-04-05 12:50:39 +0200

    Exercice 8

--- src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
+++ src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
@@ -2,6 +2,7 @@ package fr.polytech.sim.cycling;
 
 import fr.polytech.sim.Simulation;
 import fr.polytech.sim.log.Logger;
+import fr.polytech.sim.utils.Context;
 
 /**
  * Bike simulation.
@@ -10,7 +11,8 @@ public class BikeSimulator implements Simulation {
     private final Logger logger = Logger.useLogger("BikeSimulator");
 
     public void run() {
-        Bike bike = new TagAlongBike();
+
+        Bike bike = Context.inject(Bike.class);
         this.logger.log("Bike's speed %.2f Km/h.", bike.getVelocity());
         this.logger.log("Bike's mass %.2f Kg.", bike.getMass());
     }
