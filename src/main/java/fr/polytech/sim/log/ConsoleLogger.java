4fc7c78fc9d23a72585c4f5618bbd1218dfdf364
BOUCHENOT GAELLE p1935539 <gaelle.bouchenot@etu.univ-lyon1.fr>
2024-04-05 11:36:32 +0200

    Question 5

--- src/main/java/fr/polytech/sim/log/ConsoleLogger.java
+++ src/main/java/fr/polytech/sim/log/ConsoleLogger.java
@@ -15,9 +15,8 @@ public class ConsoleLogger extends NamedLogger {
     }
 
     @Override
-    public void log(String format, Object... args) {
-        String entry = String.format(format, args);
-        String message = String.format("%s\t%s\n", this.name, entry);
+    public void Output(String message)
+    {
         System.out.print(message);
     }
 }
